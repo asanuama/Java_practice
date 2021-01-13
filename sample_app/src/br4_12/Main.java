6185e6738ae9ef923ccf0c4b5e9c617d4fd71922
asanuma <moeko.a980@gmail.com>
2021-01-13 21:59:55 +0900

    [add]br4-12

---
+++ sample_app/src/br4_12/Main.java
@@ -0,0 +1,13 @@
+package br4_12;
+
+public class Main {
+    public static void main(String[] args) {
+    	int[] array = {0,2,4,6};
+    	for(int e : array) {
+    		System.out.println(e +" ");
+    	}
+    }
+
+}
+
+//拡張for 配列やインスタンスの集合から要素を一つずつ取り出し処理
