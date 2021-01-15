ac52b095ce99ae3c9fe43a62600263a6bc8c4844
asanuma <moeko.a980@gmail.com>
2021-01-15 19:48:01 +0900

    [add]br6_17

---
+++ sample_app/src/br6_17/Food.java
@@ -0,0 +1,13 @@
+package br6_17;
+
+public class Food {
+	private String name;
+
+	public Food(String n) {
+		name = n;
+	}
+
+	public void print() {
+		System.out.println(name);
+	}
+ }
---
+++ sample_app/src/br6_17/Main.java
@@ -0,0 +1,13 @@
+package br6_17;
+
+public class Main {
+	public static void main(String[] args) {
+		Food f = new Food("a food");
+//		f.name = "b food";
+		f.print();
+	}
+
+}
+//Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+//	フィールド Food.name は不可視です
+//	at br6_17.Main.main(Main.java:6) (6行目表示の状態)
