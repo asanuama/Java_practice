3ae6e9b45cc6ba5f1ac5204f6fe062a57a510fd1
asanuma <moeko.a980@gmail.com>
2021-01-18 14:20:36 +0900

    [add]br6_23

---
+++ sample_app/bronze/br6_23/ClassA.java
@@ -0,0 +1,9 @@
+package br6_23;
+
+public class ClassA {
+//	private int number = 0;
+
+//	public static int getNumber() {
+//		return number;
+//	}
+}
---
+++ sample_app/bronze/br6_23/Main.java
@@ -0,0 +1,12 @@
+package br6_23;
+
+public class Main {
+
+//	public static void main(String[] args) {
+//		int n = ClassA.getNumber();
+//		System.out.println(n);
+//	}
+
+}
+//staticメソッドからprivateフィールドへのアクセス不可。フィールドをstaticにすれば実行可
+//非 static フィールド number を static 参照できません
