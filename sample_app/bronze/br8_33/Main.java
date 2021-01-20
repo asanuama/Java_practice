601f006f8f64bb6730b3bda70997c1dd1b5bcbce
asanuma <moeko.a980@gmail.com>
2021-01-20 17:17:22 +0900

    [add]br8_33

---
+++ sample_app/bronze/br8_33/Main.java
@@ -0,0 +1,16 @@
+package br8_33;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			++i;
+			System.out.println("test");
+		}while(i<3);
+
+	}
+
+}
+//do_while文　ループを継続するかどうかを、処理の後に判定する「後判定」
+//そのため条件にかかわらず必ず1回は処理が行われる
