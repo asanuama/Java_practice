91c2e6c585d26d141769cdb1a02a4963488ba39c
asanuma <moeko.a980@gmail.com>
2021-01-24 22:55:30 +0900

    [add]sil6_20

---
+++ sample_app/silver/sil6_20/Main.java
@@ -0,0 +1,11 @@
+package sil6_20;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.Sample();
+
+	}
+
+}
---
+++ sample_app/silver/sil6_20/Sample.java
@@ -0,0 +1,8 @@
+package sil6_20;
+
+public class Sample {
+	void Sample() {
+		System.out.println("test");
+	}
+}
+//上記のように戻り値を入れた場合、コンストラクタではなく通常のメソッドとしてみなされる
