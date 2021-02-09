33aa95a728b5a685f738ae7a36e6f3f823344787
asanuma <moeko.a980@gmail.com>
2021-02-10 08:42:46 +0900

    [add]sil5_1

---
+++ sample_app/silver/sil5_1/Main.java
@@ -0,0 +1,13 @@
+package sil5_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] array;
+		array= new int[0];
+		System.out.println(array);
+
+	}
+
+}
+//[I@515f550a　ハッシュコード
